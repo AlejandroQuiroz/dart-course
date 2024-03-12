c1d43c76c0ceb586d7d4069cd44d051e5b80d021
Alejandro Quiroz <aleexquiiroz@outlook.com>
2024-03-12 14:51:43 -0600

    Editing second class

--- 01_hello_world.dart
+++ 01_hello_world.dart
@@ -1,3 +1,23 @@
 void main() {
-  print('Hello world!');
+
+  // Definir variables
+  //var myName = 'Alejandro';
+  String myName = 'Alejandro';
+
+  // Constante tipo string que no puede cambiar su valor
+  final String world = 'world';
+
+  // Inicializar variable tardía
+  late final String lastName;
+
+  lastName = 'Quiroz';
+
+  // final vs const
+  // https://www.linkedin.com/pulse/title-understanding-final-const-dart-when-use-each-neha-tanwar/
+
+  // Interpolación de strings
+  print('Hello $world, my name is $myName $lastName!');
+  print('Hello $world, my name is ${ myName.toUpperCase() } ${ lastName.toUpperCase() }!');
+  print('Suma de 1 + 1 = ${ 1 + 1 }');
+
 }
